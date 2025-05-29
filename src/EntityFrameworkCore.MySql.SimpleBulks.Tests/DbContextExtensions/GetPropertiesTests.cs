5344aae28aad5a2e1bc86089e24575c3494e4490
Phong Nguyen <nguyendoanphong.it@gmail.com>
2025-05-29 16:52:45 +0700

    Cache Properties

--- src/EntityFrameworkCore.MySql.SimpleBulks.Tests/DbContextExtensions/GetPropertiesTests.cs
+++ src/EntityFrameworkCore.MySql.SimpleBulks.Tests/DbContextExtensions/GetPropertiesTests.cs
@@ -47,4 +47,18 @@ public class GetPropertiesTests
         Assert.False(versionProperty.IsPrimaryKey);
         Assert.True(versionProperty.IsRowVersion);
     }
+
+    [Fact]
+    public void GetProperties_ShouldReturnFromCache()
+    {
+        // Arrange
+        var dbContext = GetDbContext("Tests", "");
+
+        // Act
+        var properties1 = dbContext.GetProperties(typeof(ConfigurationEntry));
+        var properties2 = dbContext.GetProperties(typeof(ConfigurationEntry));
+
+        // Assert
+        Assert.Equal(properties1, properties2);
+    }
 }
