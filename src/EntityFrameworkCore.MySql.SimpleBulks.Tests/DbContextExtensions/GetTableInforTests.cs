c75ccd030adccdac88ac8723d01d3066a81d99f5
Phong Nguyen <nguyendoanphong.it@gmail.com>
2025-05-30 09:04:48 +0700

    Cache TableInfo

---
+++ src/EntityFrameworkCore.MySql.SimpleBulks.Tests/DbContextExtensions/GetTableInforTests.cs
@@ -0,0 +1,45 @@
+﻿using EntityFrameworkCore.MySql.SimpleBulks.Extensions;
+using EntityFrameworkCore.MySql.SimpleBulks.Tests.Database;
+
+namespace EntityFrameworkCore.MySql.SimpleBulks.Tests.DbContextExtensions;
+
+public class GetTableInforTests
+{
+    protected string GetConnectionString(string dbPrefixName)
+    {
+        return $"server=localhost;database={dbPrefixName}.{Guid.NewGuid()};user=root;password=mysql;AllowLoadLocalInfile=true";
+    }
+
+    protected TestDbContext GetDbContext(string dbPrefixName, string schema)
+    {
+        return new TestDbContext(GetConnectionString(dbPrefixName), schema);
+    }
+
+    [Fact]
+    public void GetTableInfor_ReturnsCorrectTableInformation()
+    {
+        // Arrange
+        var dbContext = GetDbContext("Tests", "");
+
+        // Act
+        var tableInfor = dbContext.GetTableInfor(typeof(ConfigurationEntry));
+
+        // Assert
+        Assert.Equal("ConfigurationEntry", tableInfor.Name);
+        Assert.Equal("`ConfigurationEntry`", tableInfor.SchemaQualifiedTableName);
+    }
+
+    [Fact]
+    public void GetTableInfor_ReturnsFromCache()
+    {
+        // Arrange
+        var dbContext = GetDbContext("Tests", "");
+
+        // Act
+        var tableInfor1 = dbContext.GetTableInfor(typeof(ConfigurationEntry));
+        var tableInfor2 = dbContext.GetTableInfor(typeof(ConfigurationEntry));
+
+        // Assert
+        Assert.Equal(tableInfor1, tableInfor2);
+    }
+}
