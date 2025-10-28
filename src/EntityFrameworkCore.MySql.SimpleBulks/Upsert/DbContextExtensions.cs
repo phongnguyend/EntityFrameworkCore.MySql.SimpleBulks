69d6b9d998c4710e118f31452ac11cb8f4df7ece
Phong Nguyen <nguyendoanphong.it@gmail.com>
2025-10-28 10:38:55 +0700

    Upsert

---
+++ src/EntityFrameworkCore.MySql.SimpleBulks/Upsert/DbContextExtensions.cs
@@ -0,0 +1,68 @@
+﻿using EntityFrameworkCore.MySql.SimpleBulks.BulkMerge;
+using EntityFrameworkCore.MySql.SimpleBulks.Extensions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace EntityFrameworkCore.MySql.SimpleBulks.Upsert;
+
+public static class DbContextExtensions
+{
+    public static BulkMergeResult Upsert<T>(this DbContext dbContext, T data, Expression<Func<T, object>> idSelector, Expression<Func<T, object>> updateColumnNamesSelector, Expression<Func<T, object>> insertColumnNamesSelector, Action<BulkMergeOptions> configureOptions = null)
+    {
+        var connection = dbContext.GetMySqlConnection();
+        var transaction = dbContext.GetCurrentMySqlTransaction();
+        var outputIdColumn = dbContext.GetOutputId(typeof(T))?.PropertyName;
+
+        return new BulkMergeBuilder<T>(connection, transaction)
+            .WithId(idSelector)
+            .WithUpdateColumns(updateColumnNamesSelector)
+            .WithInsertColumns(insertColumnNamesSelector)
+            .WithDbColumnMappings(dbContext.GetColumnNames(typeof(T)))
+            .WithDbColumnTypeMappings(dbContext.GetColumnTypes(typeof(T)))
+            .WithValueConverters(dbContext.GetValueConverters(typeof(T)))
+            .WithOutputId(outputIdColumn)
+            .ToTable(dbContext.GetTableInfor(typeof(T)))
+            .ConfigureBulkOptions(configureOptions)
+            .SingleMerge(data);
+    }
+
+    public static BulkMergeResult Upsert<T>(this DbContext dbContext, T data, string idColumn, IEnumerable<string> updateColumnNames, IEnumerable<string> insertColumnNames, Action<BulkMergeOptions> configureOptions = null)
+    {
+        var connection = dbContext.GetMySqlConnection();
+        var transaction = dbContext.GetCurrentMySqlTransaction();
+        var outputIdColumn = dbContext.GetOutputId(typeof(T))?.PropertyName;
+
+        return new BulkMergeBuilder<T>(connection, transaction)
+            .WithId(idColumn)
+            .WithUpdateColumns(updateColumnNames)
+            .WithInsertColumns(insertColumnNames)
+            .WithDbColumnMappings(dbContext.GetColumnNames(typeof(T)))
+            .WithDbColumnTypeMappings(dbContext.GetColumnTypes(typeof(T)))
+            .WithValueConverters(dbContext.GetValueConverters(typeof(T)))
+            .WithOutputId(outputIdColumn)
+            .ToTable(dbContext.GetTableInfor(typeof(T)))
+            .ConfigureBulkOptions(configureOptions)
+            .SingleMerge(data);
+    }
+
+    public static BulkMergeResult Upsert<T>(this DbContext dbContext, T data, IEnumerable<string> idColumns, IEnumerable<string> updateColumnNames, IEnumerable<string> insertColumnNames, Action<BulkMergeOptions> configureOptions = null)
+    {
+        var connection = dbContext.GetMySqlConnection();
+        var transaction = dbContext.GetCurrentMySqlTransaction();
+        var outputIdColumn = dbContext.GetOutputId(typeof(T))?.PropertyName;
+
+        return new BulkMergeBuilder<T>(connection, transaction)
+            .WithId(idColumns)
+            .WithUpdateColumns(updateColumnNames)
+            .WithInsertColumns(insertColumnNames)
+            .WithDbColumnMappings(dbContext.GetColumnNames(typeof(T)))
+            .WithDbColumnTypeMappings(dbContext.GetColumnTypes(typeof(T)))
+            .WithValueConverters(dbContext.GetValueConverters(typeof(T)))
+            .WithOutputId(outputIdColumn)
+            .ToTable(dbContext.GetTableInfor(typeof(T)))
+            .ConfigureBulkOptions(configureOptions)
+            .SingleMerge(data);
+    }
+}
---
+++ src/EntityFrameworkCore.MySql.SimpleBulks/Upsert/MySqlConnectionExtensions.cs
@@ -0,0 +1,82 @@
+﻿using EntityFrameworkCore.MySql.SimpleBulks.BulkMerge;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace EntityFrameworkCore.MySql.SimpleBulks.Upsert;
+
+public static class MySqlConnectionExtensions
+{
+    public static BulkMergeResult Upsert<T>(this MySqlConnection connection, T data, Expression<Func<T, object>> idSelector, Expression<Func<T, object>> updateColumnNamesSelector, Expression<Func<T, object>> insertColumnNamesSelector, Action<BulkMergeOptions> configureOptions = null)
+    {
+        var table = TableMapper.Resolve(typeof(T));
+
+        return new BulkMergeBuilder<T>(connection)
+            .WithId(idSelector)
+            .WithUpdateColumns(updateColumnNamesSelector)
+            .WithInsertColumns(insertColumnNamesSelector)
+            .ToTable(table)
+            .ConfigureBulkOptions(configureOptions)
+            .SingleMerge(data);
+    }
+
+    public static BulkMergeResult Upsert<T>(this MySqlConnection connection, T data, string idColumn, IEnumerable<string> updateColumnNames, IEnumerable<string> insertColumnNames, Action<BulkMergeOptions> configureOptions = null)
+    {
+        var table = TableMapper.Resolve(typeof(T));
+
+        return new BulkMergeBuilder<T>(connection)
+            .WithId(idColumn)
+            .WithUpdateColumns(updateColumnNames)
+            .WithInsertColumns(insertColumnNames)
+            .ToTable(table)
+            .ConfigureBulkOptions(configureOptions)
+            .SingleMerge(data);
+    }
+
+    public static BulkMergeResult Upsert<T>(this MySqlConnection connection, T data, IEnumerable<string> idColumns, IEnumerable<string> updateColumnNames, IEnumerable<string> insertColumnNames, Action<BulkMergeOptions> configureOptions = null)
+    {
+        var table = TableMapper.Resolve(typeof(T));
+
+        return new BulkMergeBuilder<T>(connection)
+            .WithId(idColumns)
+            .WithUpdateColumns(updateColumnNames)
+            .WithInsertColumns(insertColumnNames)
+            .ToTable(table)
+            .ConfigureBulkOptions(configureOptions)
+            .SingleMerge(data);
+    }
+
+    public static BulkMergeResult Upsert<T>(this MySqlConnection connection, T data, TableInfor table, Expression<Func<T, object>> idSelector, Expression<Func<T, object>> updateColumnNamesSelector, Expression<Func<T, object>> insertColumnNamesSelector, Action<BulkMergeOptions> configureOptions = null)
+    {
+        return new BulkMergeBuilder<T>(connection)
+            .WithId(idSelector)
+            .WithUpdateColumns(updateColumnNamesSelector)
+            .WithInsertColumns(insertColumnNamesSelector)
+            .ToTable(table)
+            .ConfigureBulkOptions(configureOptions)
+            .SingleMerge(data);
+    }
+
+    public static BulkMergeResult Upsert<T>(this MySqlConnection connection, T data, TableInfor table, string idColumn, IEnumerable<string> updateColumnNames, IEnumerable<string> insertColumnNames, Action<BulkMergeOptions> configureOptions = null)
+    {
+        return new BulkMergeBuilder<T>(connection)
+            .WithId(idColumn)
+            .WithUpdateColumns(updateColumnNames)
+            .WithInsertColumns(insertColumnNames)
+            .ToTable(table)
+            .ConfigureBulkOptions(configureOptions)
+            .SingleMerge(data);
+    }
+
+    public static BulkMergeResult Upsert<T>(this MySqlConnection connection, T data, TableInfor table, IEnumerable<string> idColumns, IEnumerable<string> updateColumnNames, IEnumerable<string> insertColumnNames, Action<BulkMergeOptions> configureOptions = null)
+    {
+        return new BulkMergeBuilder<T>(connection)
+            .WithId(idColumns)
+            .WithUpdateColumns(updateColumnNames)
+            .WithInsertColumns(insertColumnNames)
+            .ToTable(table)
+            .ConfigureBulkOptions(configureOptions)
+            .SingleMerge(data);
+    }
+}
